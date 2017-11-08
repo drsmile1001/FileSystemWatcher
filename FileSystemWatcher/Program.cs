6da841b6c4edb809981fb039ef9ba115458f694e
drsmile <drsmile.1001@gmail.com>
2017-11-08 09:47:43 +0800

    完成一版

--- FileSystemWatcher/Program.cs
+++ FileSystemWatcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileSystemWatcher
 {
@@ -6,7 +7,41 @@ namespace FileSystemWatcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path;
+            do
+            {
+                Console.WriteLine("輸入要監視的資料夾:");
+                path = Console.ReadLine();
+            } while (!Directory.Exists(path));
+
+            using (var watcher = new System.IO.FileSystemWatcher
+            {
+                Path = path,
+                NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true
+            })
+            {
+                watcher.Changed += OnChanged;
+                watcher.Created += OnChanged;
+                watcher.Deleted += OnChanged;
+                watcher.Renamed += OnRenamed;
+                Console.WriteLine("開始監視，按q離開");
+                while (Console.Read() != 'q')
+                {
+                }
+            }
+            
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs renamedEventArgs)
+        {
+            Console.WriteLine($"[Renamed] {renamedEventArgs.OldFullPath} -> {renamedEventArgs.FullPath}");
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs eventArgs)
+        {
+            Console.WriteLine($"[{eventArgs.ChangeType}] {eventArgs.FullPath}");
         }
     }
 }
