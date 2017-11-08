9a94989a8892516f85be82ac3a850fd5139cb6c0
drsmile <drsmile.1001@gmail.com>
2017-11-08 11:23:39 +0800

    加入時間

--- FileSystemWatcher/Program.cs
+++ FileSystemWatcher/Program.cs
@@ -26,22 +26,22 @@ namespace FileSystemWatcher
                 watcher.Created += OnChanged;
                 watcher.Deleted += OnChanged;
                 watcher.Renamed += OnRenamed;
+                
                 Console.WriteLine("開始監視，按q離開");
                 while (Console.Read() != 'q')
                 {
                 }
             }
-            
         }
 
         private static void OnRenamed(object sender, RenamedEventArgs renamedEventArgs)
         {
-            Console.WriteLine($"[Renamed] {renamedEventArgs.OldFullPath} -> {renamedEventArgs.FullPath}");
+            Console.WriteLine($"{DateTime.Now:yy-MM-dd HH:mm:ss} [Renamed] {renamedEventArgs.OldFullPath} -> {renamedEventArgs.FullPath}");
         }
 
         private static void OnChanged(object sender, FileSystemEventArgs eventArgs)
         {
-            Console.WriteLine($"[{eventArgs.ChangeType}] {eventArgs.FullPath}");
+            Console.WriteLine($"{DateTime.Now:yy-MM-dd HH:mm:ss} [{eventArgs.ChangeType}] {eventArgs.FullPath}");
         }
     }
 }
