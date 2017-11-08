d2bdbd3e02b5271d0fda74e72d659d5d8a0ead57
drsmile <drsmile.1001@gmail.com>
2017-11-08 09:16:08 +0800

    加入專案檔案。

---
+++ FileSystemWatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSystemWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
